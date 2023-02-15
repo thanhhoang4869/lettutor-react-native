95b7176871c5f58beac690943a59bf82536129d9
Thanh Hoang <thanhhoang4869@gmail.com>
2023-02-15 16:15:45 +0700

    add register button, forgot pass button. TODO: add icons

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {TextInput} from '@react-native-material/core';
 import {Button, Flex, WhiteSpace} from '@ant-design/react-native';
 import {useAppTheme} from '../../../App';
@@ -11,18 +11,25 @@ export function LoginPage(): JSX.Element {
     imageStyle: {
       alignSelf: 'center',
       width: '70%',
+      marginTop: -40,
     },
     container: {
       backgroundColor: 'white',
       height: '100%',
       padding: 30,
-      marginTop: -40,
     },
     button: {
       width: '100%',
     },
     input: {
       width: '100%',
+    },
+    text: {
+      fontWeight: 'bold',
+      color: theme?.colors?.primary,
+    },
+    marginRight: {
+      marginRight: 5,
     },
   });
 
@@ -35,7 +42,7 @@ export function LoginPage(): JSX.Element {
       <WhiteSpace />
 
       <TextInput
-        label="Username"
+        label="Email"
         variant="outlined"
         style={style.input}
         color={theme?.colors?.primary}
@@ -52,10 +59,33 @@ export function LoginPage(): JSX.Element {
       />
 
       <WhiteSpace />
+      <WhiteSpace />
 
       <Button type="primary" style={style.button}>
         Login
       </Button>
+
+      <WhiteSpace />
+      <WhiteSpace />
+
+      <TouchableOpacity>
+        <Text style={style.text}>Forgot password?</Text>
+      </TouchableOpacity>
+
+      <WhiteSpace />
+      <WhiteSpace />
+
+      <Text>Or continue with</Text>
+
+      <WhiteSpace />
+      <WhiteSpace />
+
+      <Flex>
+        <Text style={style.marginRight}>Not a member yet?</Text>
+        <TouchableOpacity>
+          <Text style={style.text}>Register</Text>
+        </TouchableOpacity>
+      </Flex>
     </Flex>
   );
 }
