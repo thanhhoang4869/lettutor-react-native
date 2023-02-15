83d8eb9ccc506b4c4eb955405acbba5e1ec6c612
Thanh Hoang <thanhhoang4869@gmail.com>
2023-02-15 16:02:19 +0700

    login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,22 +1,63 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
+import {TextInput} from '@react-native-material/core';
+import {Button, Flex, WhiteSpace} from '@ant-design/react-native';
+import {useAppTheme} from '../../../App';
 
-class LoginPage extends React.Component {
-  render() {
-    return (
-      <View>
-        {/* <Image
-          source={require('../../assets/images/logo.png')}
-          style={styles.logo}
-        /> */}
-        <Text>Welcome to the app</Text>
-        <Text>Login to continue</Text>
-        <TouchableOpacity>
-          <Text>Login</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+export function LoginPage(): JSX.Element {
+  const theme = useAppTheme();
+
+  const style = StyleSheet.create({
+    imageStyle: {
+      alignSelf: 'center',
+      width: '70%',
+    },
+    container: {
+      backgroundColor: 'white',
+      height: '100%',
+      padding: 30,
+      marginTop: -40,
+    },
+    button: {
+      width: '100%',
+    },
+    input: {
+      width: '100%',
+    },
+  });
+
+  const logo = require('../../assets/logo.png');
+
+  return (
+    <Flex justify="center" direction="column" style={style.container}>
+      <Image style={style.imageStyle} resizeMode={'contain'} source={logo} />
+
+      <WhiteSpace />
+
+      <TextInput
+        label="Username"
+        variant="outlined"
+        style={style.input}
+        color={theme?.colors?.primary}
+      />
+
+      <WhiteSpace />
+
+      <TextInput
+        label="Password"
+        variant="outlined"
+        style={style.input}
+        color={theme?.colors?.primary}
+        secureTextEntry={true}
+      />
+
+      <WhiteSpace />
+
+      <Button type="primary" style={style.button}>
+        Login
+      </Button>
+    </Flex>
+  );
 }
 
 export default LoginPage;
