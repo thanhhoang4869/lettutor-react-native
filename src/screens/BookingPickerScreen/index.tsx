f3a9c004394609ddbfead23eaeba50a10dca4d70
Thanh Hoang <thanhhoang4869@gmail.com>
2023-03-01 22:25:40 +0700

    add onfirm booking modal

--- src/screens/BookingPickerScreen/index.tsx
+++ src/screens/BookingPickerScreen/index.tsx
@@ -1,10 +1,18 @@
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import CalendarStrip from 'react-native-calendar-strip';
 import {color, style} from 'style';
 import {WhiteSpace, Flex} from '@ant-design/react-native';
-import {Button} from 'galio-framework';
+import {Button, Input} from 'galio-framework';
 import {RadioButton} from 'react-native-paper';
+import {Icon} from 'react-native-elements';
+import Modal from 'react-native-modal';
 
 const styles = StyleSheet.create({
   container: {flex: 1, height: '100%', backgroundColor: 'white'},
@@ -25,8 +33,73 @@ const styles = StyleSheet.create({
 const BookingPickerScreen = ({navigation: {navigate}}: any) => {
   const [value, setValue] = React.useState('first');
 
+  const [isConfirmModalVisible, setConfirmModalVisible] = React.useState(false);
+  const toggleConfirmModal = () => {
+    setConfirmModalVisible(!isConfirmModalVisible);
+  };
+
   return (
     <View style={styles.container}>
+      {/* rp modal start */}
+      <View>
+        <Modal isVisible={isConfirmModalVisible}>
+          <Flex style={style.modal} direction="column" align="start">
+            <Text style={{margin: 5, ...style.modalTitle}}>
+              Confirm booking
+            </Text>
+
+            <WhiteSpace size="lg" />
+
+            <Text
+              style={{
+                fontWeight: 'bold',
+                marginLeft: 5,
+              }}>
+              Your booking is at: 22/02/2023 - 18:30
+            </Text>
+
+            <WhiteSpace size="xl" />
+
+            <Flex align="center">
+              <Icon
+                name="report"
+                size={25}
+                type="material-icons"
+                color={color.primaryColor}
+              />
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  marginLeft: 5,
+                }}>
+                Note for this booking
+              </Text>
+            </Flex>
+
+            <WhiteSpace size="lg" />
+
+            <Input cursorColor={color.primaryColor} style={style.textArea} />
+
+            <WhiteSpace size="lg" />
+
+            <Flex justify="between" style={{width: '100%', marginLeft: 10}}>
+              <TouchableOpacity
+                onPress={() => {
+                  toggleConfirmModal();
+                }}>
+                <Text style={style.textBold}>Cancel</Text>
+              </TouchableOpacity>
+              <Button
+                style={style.primaryButtonNoWidth}
+                onPress={toggleConfirmModal}>
+                Confirm
+              </Button>
+            </Flex>
+          </Flex>
+        </Modal>
+      </View>
+      {/* rp modal end */}
+
       <Text style={styles.pageTitle}>Booking time</Text>
 
       <CalendarStrip
@@ -78,8 +151,8 @@ const BookingPickerScreen = ({navigation: {navigate}}: any) => {
         <WhiteSpace size="lg" />
         <WhiteSpace size="lg" />
 
-        <Button style={styles.button} onPress={() => navigate('BookingDetail')}>
-          Next
+        <Button style={styles.button} onPress={toggleConfirmModal}>
+          Book class
         </Button>
       </View>
     </View>
